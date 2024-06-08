6f676719d36c1882fc8bd97ee7ef128f95c54fee
kibria-khandaker <kibriakhandaker@gmail.com>
2024-06-08 19:58:12 +0600

    Add some css file and readme.txt file

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,18 +5,23 @@ function navaz_css_js_file_call() {
     wp_enqueue_style( 'navaz-style', get_stylesheet_uri() );
     wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), 'v5.3.3', 'all' );
     wp_register_style( 'theme_custom', get_template_directory_uri() . '/css/theme_custom.css', array(), '1.0.0', 'all' );
-    wp_register_style( 'responsive', get_template_directory_uri() . '/css/responsive.css', array(), '1.0.1', 'all' );
+    wp_register_style( 'responsive', get_template_directory_uri() . '/css/responsive.css', array(), '1.0.0', 'all' );
 
     wp_enqueue_style( 'bootstrap' );
     wp_enqueue_style( 'theme_custom' );
     wp_enqueue_style( 'responsive' );
-
 
     // JS
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), 'v5.3.3', true );
     wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
 
-
 }
-add_action( 'wp_enqueue_scripts', 'navaz _css_js_file_call' );
+add_action( 'wp_enqueue_scripts', 'navaz_css_js_file_call' );
+
+
+// Google call
+function netkib_google_fonts() {
+    wp_enqueue_style( 'netkib_google_fonts', 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap', false );
+}
+add_action( 'wp_enqueue_scripts', 'netkib_google_fonts' );
